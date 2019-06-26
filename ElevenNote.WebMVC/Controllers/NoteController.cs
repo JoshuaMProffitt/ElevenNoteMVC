a9e2f2fc92f729a692b0900e558f9dee1c7ba25b
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-06-26 11:55:28 -0400

    NoteListItem Model

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+
+                 //1
+    public class NoteController : Controller
+    {
+         //2                  //3
+        // GET: Note
+        public ActionResult Index()
+        {
+                      //4
+            return View();
+        }
+    }
+}
